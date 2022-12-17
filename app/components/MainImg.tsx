a159f5b956a130133dad87455000a6e6e8f96b42
Luong Tuan anh <cuti1mat@gmail.com>
2022-12-17 16:24:17 +0700

    responsive for smaller phone

--- app/components/MainImg.tsx
+++ app/components/MainImg.tsx
@@ -5,16 +5,16 @@ export default function MainImg({size, className}: {size?: "sm" | "base" | "lg",
 
     switch (size) {
         case "sm":
-            tailwindSize = "w-20"
+            tailwindSize = "w-20 h-20"
             break
         case "base":
-            tailwindSize = "w-80"
+            tailwindSize = "w-80 h-80"
             break
         case "lg":
-            tailwindSize = "w-96"
+            tailwindSize = "w-96 h-96"
             break
         default:
-            tailwindSize = "w-48"
+            tailwindSize = "w-48 h-48"
     }
     return (
         <img className={`${tailwindSize} object-cover min-h-0 min-w-0 rounded-full mb-2 block mx-auto ${className}`}
