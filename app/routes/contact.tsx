215bb51a961884071990e53039785b353089bdbe
Luong Tuan anh <cuti1mat@gmail.com>
2022-12-17 16:55:52 +0700

    change color

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -14,7 +14,7 @@ export default function Contact() {
                 <MainImg size={"sm"}></MainImg>
                 <Button className={"mx-auto my-4"} disabled={true}>Contact</Button>
                 <h2 className={"text-center font-bold text-2xl mt-4 mb-2"}>Get In Touch With Me</h2>
-                <IoLogoOctocat className={"mb-6 w-full text-5xl text-sky-500"}/>
+                <IoLogoOctocat className={"mb-6 w-full text-5xl text-indigo-500"}/>
                 <div className={"grid grid-cols-2 gap-y-8 gap-x-2"}>
                     <ContactIcon link={"instagram.com/tuan.anhlg"} icon={<AiOutlineInstagram/>}>tuan.anhlg</ContactIcon>
                     <ContactIcon link={"facebook.com/vit.conn"} icon={<MdOutlineFacebook/>}>vit.conn</ContactIcon>
