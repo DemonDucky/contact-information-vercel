770f878e407ea89d92bcb3db86c8d2ef9de41fbc
Luong Tuan anh <cuti1mat@gmail.com>
2022-12-17 16:49:56 +0700

    change some icon

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -2,7 +2,7 @@ import NavBar from "~/components/NavBar";
 import MainLayout from "~/layouts/MainLayout";
 import MainImg from "~/components/MainImg";
 import Button from "~/components/Button";
-import {IoHeart} from "react-icons/io5";
+import {IoLogoOctocat} from "react-icons/io5";
 import {AiOutlineInstagram, AiOutlineMail, AiOutlineGithub} from "react-icons/ai";
 import {MdOutlineFacebook} from "react-icons/md";
 import ContactIcon from "~/components/ContactIcon";
@@ -13,8 +13,8 @@ export default function Contact() {
             <div className={"justify-center flex flex-col grow"}>
                 <MainImg size={"sm"}></MainImg>
                 <Button className={"mx-auto my-4"} disabled={true}>Contact</Button>
-                <h3 className={"text-center font-bold text-2xl mt-4 mb-2"}>Get In Touch With Me</h3>
-                <IoHeart className={"mb-6 w-full text-5xl text-red-600"}/>
+                <h2 className={"text-center font-bold text-2xl mt-4 mb-2"}>Get In Touch With Me</h2>
+                <IoLogoOctocat className={"mb-6 w-full text-5xl text-sky-500"}/>
                 <div className={"grid grid-cols-2 gap-y-8 gap-x-2"}>
                     <ContactIcon link={"instagram.com/tuan.anhlg"} icon={<AiOutlineInstagram/>}>tuan.anhlg</ContactIcon>
                     <ContactIcon link={"facebook.com/vit.conn"} icon={<MdOutlineFacebook/>}>vit.conn</ContactIcon>
